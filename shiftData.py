f463f8be4693ab9bd28156eebad02446e3443ac6
Liam OBrien <lob.obuk@gmail.com>
2021-03-31 17:11:18 +0100

    Ammended data

---
+++ shiftData.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+
+df = pd.read_csv('AllCrimeSummed.csv')
+
+shiftLat = 0.003 / 2
+shiftLon = 0.003 / 2
+
+
+df['lat'] += shiftLat
+df['lon'] += shiftLon
+
+
+df.to_csv('AllCrimeSummed.csv', index=False)
+
+import os
+os.system('git add .')
+os.system('git commit -am "Ammended data"')
+os.system('git push origin main')
+
